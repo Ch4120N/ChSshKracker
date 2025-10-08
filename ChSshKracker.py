625c416db81f66df3287bc669c25a550dbe2481a
Ch4120N <charonsecagency@gmail.com>
2025-10-08 20:11:19 +0330

    Updated requirements.txt, ChSshKracker.py

---
+++ ChSshKracker.py
@@ -0,0 +1,18 @@
+# -*- UTF-8 -*-
+# ChSshKracker.py
+
+import os
+import sys
+
+try:
+    import paramiko
+    from colorama import Fore, init
+    from prompt_toolkit import prompt
+    init(autoreset=True)
+except ImportError:
+    sys.exit('\n [ - ] Please install requirements libraries. Using commands below:\n' \
+             '\t- python -m pip install paramiko==3.4.0 colorama==0.4.6 prompt_toolkit==3.0.52\n' \
+             '\t- Or\n' \
+             '\t- python -m pip install -r requirements.txt\n'
+            )
+
