c03f2ececcd6487af1728c242a2a0e1129733ab7
Ch4120N <charonsecagency@gmail.com>
2025-10-08 19:08:04 +0330

    Updated decorators.py

--- ui/decorators.py
+++ ui/decorators.py
@@ -27,20 +27,19 @@ class StatusDecorator:
 
 class MessageDecorator(StatusDecorator):
     def InfoMessage(self, RequestMessage):
-        print(self.INFO + Style.RESET_ALL + RequestMessage)
+        print(self.INFO + Style.BRIGHT + Fore.WHITE + RequestMessage + Style.RESET_ALL)
     
     def SuccessMessage(self, RequestMessage):
-        print(self.PASS + Style.RESET_ALL + RequestMessage)
+        print(self.PASS + Style.BRIGHT + Fore.GREEN + RequestMessage + Style.RESET_ALL)
 
     def FailureMessage(self, RequestMessage):
-        print(self.FAIL + Style.RESET_ALL + RequestMessage)
+        print(self.FAIL + Style.BRIGHT + Fore.RED + RequestMessage + Style.RESET_ALL)
 
     def WarningMessage(self, RequestMessage):
-        print(self.WARN + Style.RESET_ALL + RequestMessage)
+        print(self.WARN + Style.BRIGHT + Fore.YELLOW + RequestMessage + Style.RESET_ALL)
 
     def SectionMessage(self, RequestMessage):
-        print(self.HEAD + Fore.CYAN + Style.BRIGHT
-              + RequestMessage + Style.RESET_ALL)
+        print(self.HEAD + Style.BRIGHT + Fore.CYAN + RequestMessage + Style.RESET_ALL)
 
     def GeneralMessage(self, RequestMessage):
         print(self.STDS + Style.RESET_ALL + RequestMessage)
