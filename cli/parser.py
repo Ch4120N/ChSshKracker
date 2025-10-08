309ad51e41e70b186f43030febf78719762f7ce6
Ch4120N <charonsecagency@gmail.com>
2025-10-08 19:44:11 +0330

    Added libs

--- cli/parser.py
+++ cli/parser.py
@@ -1,3 +1,13 @@
 # -*- UTF-8 -*-
 # cli/parser.py
 
+import argparse
+from typing import Callable
+
+from core.config import (
+    __version__,
+    SCRIPT_NAME,
+    SCRIPT_DESCRIPTION,
+    globalConfig
+)
+from ui.sharp_box import SharpBox
