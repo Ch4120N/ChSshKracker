1dcd2f8d562c1f0f5b0c938236a5657267cfa962
Ch4120N <charonsecagency@gmail.com>
2025-10-08 20:11:59 +0330

    Updated ImportError

--- ChSshKracker.py
+++ ChSshKracker.py
@@ -11,7 +11,7 @@ try:
     init(autoreset=True)
 except ImportError:
     sys.exit('\n [ - ] Please install requirements libraries. Using commands below:\n' \
-             '\t- python -m pip install paramiko==3.4.0 colorama==0.4.6 prompt_toolkit==3.0.52\n' \
+             '\t- python -m pip install cryptography==40.0.2 paramiko==3.4.0 colorama==0.4.6 prompt_toolkit==3.0.52\n' \
              '\t- Or\n' \
              '\t- python -m pip install -r requirements.txt\n'
             )
