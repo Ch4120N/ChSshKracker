da7cef9fff7fc0cc1eee555f67753deac5ec2aef
Ch4120N <charonsecagency@gmail.com>
2025-11-05 17:44:28 +0330

    added required lib

--- cli/interactive.py
+++ cli/interactive.py
@@ -1,3 +1,18 @@
 # -*- UTF-8 -*-
 # cli/interactive.py
 
+import os
+import signal
+from threading import Event
+from types import FrameType
+from typing import Optional
+
+from prompt_toolkit import prompt
+from prompt_toolkit.styles import Style
+from prompt_toolkit.shortcuts.prompt import CompleteStyle
+from colorama import Fore, init
+
+init(autoreset=True)
+
+
+
