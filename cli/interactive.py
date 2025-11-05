1901c3f02521c107813cfb17463be67ab1b1c2c9
Ch4120N <charonsecagency@gmail.com>
2025-11-05 18:56:19 +0330

    fixed get ips function

--- cli/interactive.py
+++ cli/interactive.py
@@ -2,6 +2,15 @@
 # cli/interactive.py
 
 from __future__ import annotations
+from cli.path_completer import PathCompleter
+from utils.utility import utility as utils
+from ui.summary_render import SummaryRenderer, GetSummary
+from ui.decorators import MsgDCR
+from ui.banner import Banners
+from core.config import (
+    _stop_event,
+    Config
+)
 
 import os
 import signal
@@ -15,17 +24,6 @@ from prompt_toolkit.shortcuts.prompt import CompleteStyle
 from colorama import Fore, init
 
 init(autoreset=True)
-
-from core.config import (
-    _stop_event,
-    Config
-)
-
-from ui.banner import Banners
-from ui.decorators import MsgDCR
-from ui.summary_render import SummaryRenderer, GetSummary
-from utils.utility import utility as utils
-from cli.path_completer import PathCompleter
 
 
 def handle_SIGINT(frm, func):
@@ -42,12 +40,12 @@ class Inputs:
             ('class:arrow', '>'),
             ('class:brackets', ' ] ')
         ]
-    
+
     def build_input_styles(self, prompt_color: str = 'white bold') -> Style:
         return Style.from_dict({
             'prompt': 'ansired bold',
             'brackets': 'magenta bold',
-            'bluebrackets' : 'blue bold',
+            'bluebrackets': 'blue bold',
             'arrow': 'white bold',
             'text': prompt_color,
             'key': 'red bold',
@@ -57,10 +55,10 @@ class Inputs:
     def input_with_prompt(self, prompt_message, completer_on: bool = False, prompt_color: str = 'white bold'):
         try:
             return prompt(prompt_message,
-                        completer=PathCompleter() if completer_on else None,
-                        # complete_style=CompleteStyle.READLINE_LIKE, # Optional
-                        style=self.build_input_styles(prompt_color)
-                        ).strip()
+                          completer=PathCompleter() if completer_on else None,
+                          # complete_style=CompleteStyle.READLINE_LIKE, # Optional
+                          style=self.build_input_styles(prompt_color)
+                          ).strip()
         except (KeyboardInterrupt, EOFError):
             handle_SIGINT(1, None)
         except RuntimeError:
@@ -101,7 +99,7 @@ class Inputs:
             ('class:text', 'Enter the path to the IP list file: ')
         ]
         return self.input_with_prompt(self.INPUT_DECORATOR + prompt_message, completer_on=True)
-    
+
     def input_continue_with_combos(self):
         prompt_message = [
             ('class:text', 'Do you want to provide a combo file instead of separate user/password lists?'),
@@ -111,13 +109,13 @@ class Inputs:
             ('class:text', ': ')
         ]
         return self.input_with_prompt(self.INPUT_DECORATOR + prompt_message).lower()
-    
+
     def input_get_combo_file(self):
         prompt_message = [
             ('class:text', 'Enter the path to the generated combo file: ')
         ]
         return self.input_with_prompt(self.INPUT_DECORATOR + prompt_message, completer_on=True)
-    
+
     def input_get_user_file(self):
         prompt_message = [
             ('class:text', 'Enter the path to the username list file: ')
@@ -139,7 +137,7 @@ class Inputs:
             ('class:text', ': ')
         ]
         return self.input_with_prompt(self.INPUT_DECORATOR + prompt_message)
-    
+
     def input_get_max_workers(self):
         prompt_message = [
             ('class:text', 'Enter the maximum number of workers'),
@@ -149,7 +147,7 @@ class Inputs:
             ('class:text', ': ')
         ]
         return self.input_with_prompt(self.INPUT_DECORATOR + prompt_message)
-    
+
     def input_get_per_workers(self):
         prompt_message = [
             ('class:text', 'Enter the number of concurrent connections per worker'),
@@ -161,7 +159,7 @@ class Inputs:
             ('class:text', ': ')
         ]
         return self.input_with_prompt(self.INPUT_DECORATOR + prompt_message)
-    
+
     def input_confirm_configurations(self):
         prompt_message = [
             ('class:text', 'Do you want to change any other settings?'),
@@ -176,9 +174,9 @@ class Inputs:
 class InteractiveUI:
     def __init__(self):
         self.summary_obj = SummaryRenderer(
-            title='Configuration Summary', 
-            space=5, 
-            global_margin_left_spaces=2, 
+            title='Configuration Summary',
+            space=5,
+            global_margin_left_spaces=2,
             max_width=80
         )
         self.get_summary_obj = GetSummary()
@@ -212,52 +210,52 @@ class InteractiveUI:
         Config.TIMEOUT = 5
         Config.MAX_WORKERS = 25
         Config.CONCURRENT_PER_WORKER = 25
-    
+
     def _clear_file_paths(self):
         Config.IP_FILE = ''
         Config.USERNAME_FILE = ''
         Config.PASSWORD_FILE = ''
         Config.COMBO_FILE = ''
-    
+
     def _print_banner(self):
         print(Fore.LIGHTRED_EX + Banners.MainBanner(margin_left=2) + Fore.RESET, '\n')
 
     def _continue(self):
         self.inputs.input_continue()
-    
+
     def run(self):
         while not self.MAIN_EVENT.is_set():
             self.get_ips()
-            self.get_combos_or_userpass()
-            self.get_timeout()
-            self.get_max_workers()
-            self.get_per_workers()
-            self.get_confirm_configuration()
-    
+            # self.get_combos_or_userpass()
+            # self.get_timeout()
+            # self.get_max_workers()
+            # self.get_per_workers()
+            # self.get_confirm_configuration()
+
     def get_ips(self):
         while not self.REQUIRED_IP_EVENT.is_set():
             utils.clear_screen()
             self._print_banner()
 
             ip_list = self.inputs.input_get_ip_file()
-            ip_path = os.path.realpath(ip_list)
+            self.ip_path = os.path.realpath(ip_list)
 
             if (not ip_list):
                 MsgDCR.FailureMessage("IP list is required!")
                 self._continue()
                 continue
-        
+
             elif (
-                (not os.path.isfile(ip_path)) 
-                or 
-                (not os.path.exists(ip_path))
-                ):
-                MsgDCR.FailureMessage(f'IP list does not exist: {ip_path}')
+                (not os.path.isfile(self.ip_path))
+                or
+                (not os.path.exists(self.ip_path))
+            ):
+                MsgDCR.FailureMessage(
+                    f'IP list does not exist: {self.ip_path}')
                 self._continue()
                 continue
             else:
                 self.REQUIRED_IP_EVENT.set()
-        
-        SUMMARY['IP FILE'] = ip_path
-        FILES_PATH.IP_FILE = ip_path
-        
+                break
+
+        Config.IP_FILE = self.ip_path
